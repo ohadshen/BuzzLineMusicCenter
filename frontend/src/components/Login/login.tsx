271a88f0afaad3f7eeb68cd70c6422ad93532322
meyron1456 <meyron1456@gmail.com>
2023-03-16 00:47:29 +0200

    finishing JWT authentication client side

--- frontend/src/components/Login/login.tsx
+++ frontend/src/components/Login/login.tsx
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
 import "./login.css";
 import { Link, useNavigate } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import firebase from "../../services/firebase";
+import 'firebase/compat/auth';
+
+const LoginFirebase = (email: string, password: string) => {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredentials) => {
+      firebase.auth().currentUser.getIdToken().then((token) => {
+        localStorage.setItem("token", token);
+      });
+    });
+}
 
 const Login = () => {
   const navigate = useNavigate();
@@ -8,7 +20,17 @@ const Login = () => {
   const [password, setPassword] = useState("");
 
   return (
-    <div>LOGIN!</div>
+    <div>
+      login!!!
+      <Button
+              variant="secondary"
+              className="costumeBtn"
+              onClick={() => LoginFirebase("meyBoi22@gmail.com", "123123")}
+      >
+              <br />
+              Add To Cart
+            </Button>
+    </div>
   );
 };
 
---
+++ frontend/src/services/firebase.ts
@@ -0,0 +1,15 @@
+import firebase from 'firebase/compat/app';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCU0BxqK1LleTpwV4nbanT6hWhBzvGRs6A",
+    authDomain: "buzzline-93257.firebaseapp.com",
+    projectId: "buzzline-93257",
+    storageBucket: "buzzline-93257.appspot.com",
+    messagingSenderId: "382117293728",
+    appId: "1:382117293728:web:6c669a18de70db591fa3c5",
+    measurementId: "G-TDQ0FB3SY2"
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
